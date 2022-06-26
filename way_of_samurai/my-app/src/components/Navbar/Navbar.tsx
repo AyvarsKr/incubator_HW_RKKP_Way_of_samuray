a73641d745ffdd818ab1d8c31e6a18d0c7d4d15c
AyvarsKr <ayvars@bk.ru>
2022-06-26 23:59:40 +0300

    Way_of_samurai_to_20th_video

--- way_of_samurai/my-app/src/components/Navbar/Navbar.tsx
+++ way_of_samurai/my-app/src/components/Navbar/Navbar.tsx
@@ -1,26 +1,25 @@
 import s from './Navbar.module.css'
-import {NavLink} from "react-router-dom";
+
+
 
 export const Navbar = () => {
-
     return (
 <nav className={s.nav}>
-    <div className={s.item}>
-        <NavLink to="/profile" className={({ isActive }) => isActive ? s.activeLink : undefined}>Profile</NavLink>
+    <div className={`${s.item} ${s.active}`}>
+        <a href="src/components/Navbar/Navbar">Profile</a>
     </div>
     <div className={s.item}>
-        <NavLink to="/dialogs" className={({ isActive }) => isActive ? s.activeLink : undefined}>Messages</NavLink>
+        <a href="src/components/Navbar/Navbar">Messages</a>
     </div>
     <div className={s.item}>
-        <NavLink to="/dialogs" className={({ isActive }) => isActive ? s.activeLink : undefined}>News</NavLink>
+        <a href="src/components/Navbar/Navbar">News</a>
     </div>
     <div className={s.item}>
-        <a href="/Music">Music</a>
+        <a href="src/components/Navbar/Navbar">Music</a>
     </div>
     <div className={s.item}>
-        <a href="/Settings">Settings</a>
+        <a href="src/components/Navbar/Navbar">Settings</a>
     </div>
-
 </nav>
     )
 }
